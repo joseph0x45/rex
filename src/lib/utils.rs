5e799498e4c76b6ff505734eeed4f5bad019c653
TheWisePigeon <zozozozeph@gmail.com>
2022-12-03 15:47:38 +0000

    arch: added main structure

---
+++ src/lib/utils.rs
@@ -0,0 +1,4 @@
+
+pub mod utils{
+    pub static StaticVar: String = String::from("Yeah");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
+use std;
 
 
 fn main() {
-    
+
     println!("Welcome to Rex CLI 🙂");
+    let action = std::env::args().nth(1);
+    match action {
+        Some(action)=>{
+            match action.as_str() {
+                "init"=>{
+                    println!("init command received");
+                    return
+                },
+                "help"=>{
+                    println!("
+                        Welcome to the Rex documentation \n Please refeer to https://github.com/TheWisePigeon/rex/readme.md for the full extended documentation
+                    ");
+                    return
+                },
+                _=>{
+                    println!("Unrecognized command");
+                    std::process::exit(0)
+                }
+            }
+        },
+        None=>{
+            println!("Welcome to the Rex interactive scaffolder");
+            return
+        }
+    }
 }
